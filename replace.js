6d3a1bf6199b415264abaf44c49642059f923afd
Gomango999 <kv.zhu999@gmail.com>
2019-01-13 00:00:11 +1100

    Created Emote Object

--- replace.js
+++ replace.js
@@ -1,22 +1,23 @@
+
+function Emote (cmd) {
+	this.cmd = cmd;
+	this.keyword = `!${cmd}`;
+	this.filepath = `emotes/${cmd}_32.jpeg`;
+	this.url = chrome.extension.getURL(this.filepath);
+	this.imgElement = `<img src=${this.url}>`;
+}
+
+let supik = new Emote("supik");
+
+
+let fbMsg = "span._3oh-._58nk"
+
 setInterval(function(){
-	var keyword = "!supik"
-	var messageClass = "_3oh- _58nk"
-
-	var imgURL = chrome.extension.getURL("32_pik.jpeg");
-	var image = "<img src=" + imgURL + "> </img>"
-
-    var elements = document.getElementsByTagName("span")
-
-    for (var i = 0; i < elements.length; i++) {
-        text = elements[i].innerHTML;
-        if (elements[i].className == messageClass) {
-	        if (text.includes(keyword)) {
-	        	// console.log("I:", i)
-	        	// console.log(elements[i])
-	        	// console.log(text)
-	        	elements[i].innerHTML = text.replace(keyword, image);
-	        }
-	    }
-    }
+	$(fbMsg).each(function(i) {
+		if ($(this).html().includes(supik.keyword)) {
+			replaced = $(this).html().replace(supik.keyword, supik.imgElement);
+			$(this).html(replaced)
+		}
+	})
+	
 }, 2*1000);
-
