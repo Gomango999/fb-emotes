cdccaa5c3b9037872fa37995dea88969ce78dbec
Gomango999 <kv.zhu999@gmail.com>
2019-01-14 00:38:18 +1100

    Added mutation observers (doesn't work well)

--- replace.js
+++ replace.js
@@ -1,23 +1,73 @@
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
 
 function Emote (cmd) {
 	this.cmd = cmd;
 	this.keyword = `!${cmd}`;
-	this.filepath = `emotes/${cmd}_32.jpeg`;
+	this.filepath = `emotes/${cmd}_24.png`;
 	this.url = chrome.extension.getURL(this.filepath);
 	this.imgElement = `<img src=${this.url}>`;
 }
 
-let supik = new Emote("supik");
+//Define all emotes here
+let emotes = [];
+emotes.push(new Emote("supik"));
+emotes.push(new Emote("pogchamp"));
 
 
-let fbMsg = "span._3oh-._58nk"
 
-setInterval(function(){
-	$(fbMsg).each(function(i) {
-		if ($(this).html().includes(supik.keyword)) {
-			replaced = $(this).html().replace(supik.keyword, supik.imgElement);
-			$(this).html(replaced)
-		}
+let fbMsgSel = "span._3oh-._58nk";
+let messageBox = undefined;
+
+function updateAll() {
+	emotes.forEach(function (emote) {
+		$(fbMsgSel).each(function(i) {	
+			if ($(this).html().includes(emote.keyword)) {
+				replaced = $(this).html().replaceAll(emote.keyword, emote.imgElement);
+				$(this).html(replaced);
+			}
+		})
 	})
+}
+
+function updateSpan(span) {
+	emotes.forEach(function (emote) {
+		replaced = $(span).html().replaceAll(emote.keyword, emote.imgElement);
+		$(span).html(replaced);
+	})
+}
 	
-}, 2*1000);
+
+function subscriber (mutations) {
+	mutations.forEach((mutation) => {
+		//Replace the stuff in span
+		let span = mutation.target.childNodes[1].firstChild;
+		if (span.tagName != "SPAN") {
+			return
+		}
+		updateSpan(span);
+	});
+}
+
+
+//Attach observer onto the message div when it loads
+const config = {
+	attributes: true,
+	subtree: true
+};
+const observer = new MutationObserver(subscriber)
+waitLoad = setInterval(function() {
+	try {
+		messageBox = $("div.uiScrollableAreaContent")[2].firstChild.childNodes[2];
+	}
+	catch {}
+
+	if (messageBox != undefined) {
+		updateAll();
+		observer.observe(messageBox, config);
+		clearInterval(waitLoad);
+	}
+}, 1*1000)
+
