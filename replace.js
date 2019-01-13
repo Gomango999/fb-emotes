73b0226b946581963a8253cfb3dd821377910754
Gomango999 <kv.zhu999@gmail.com>
2019-01-14 00:43:02 +1100

    Reverted to interval method...

--- replace.js
+++ replace.js
@@ -16,8 +16,6 @@ let emotes = [];
 emotes.push(new Emote("supik"));
 emotes.push(new Emote("pogchamp"));
 
-
-
 let fbMsgSel = "span._3oh-._58nk";
 let messageBox = undefined;
 
@@ -30,44 +28,10 @@ function updateAll() {
 			}
 		})
 	})
-}
+}	
 
-function updateSpan(span) {
-	emotes.forEach(function (emote) {
-		replaced = $(span).html().replaceAll(emote.keyword, emote.imgElement);
-		$(span).html(replaced);
-	})
-}
-	
+setInterval(function() {
+	updateAll()
+}, 2*1000)
 
-function subscriber (mutations) {
-	mutations.forEach((mutation) => {
-		//Replace the stuff in span
-		let span = mutation.target.childNodes[1].firstChild;
-		if (span.tagName != "SPAN") {
-			return
-		}
-		updateSpan(span);
-	});
-}
-
-
-//Attach observer onto the message div when it loads
-const config = {
-	attributes: true,
-	subtree: true
-};
-const observer = new MutationObserver(subscriber)
-waitLoad = setInterval(function() {
-	try {
-		messageBox = $("div.uiScrollableAreaContent")[2].firstChild.childNodes[2];
-	}
-	catch {}
-
-	if (messageBox != undefined) {
-		updateAll();
-		observer.observe(messageBox, config);
-		clearInterval(waitLoad);
-	}
-}, 1*1000)
 
