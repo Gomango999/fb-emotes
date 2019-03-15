3c4c5c64070e5dc490fcdd0614ebaff0f8618f5f
Gomango999 <kv.zhu999@gmail.com>
2019-03-15 16:59:10 +1100

    Fixed mutation observers

--- replace.js
+++ replace.js
@@ -21,6 +21,7 @@ emotes.push(new Emote("pogchamp"));
 let fbMsgSel = "span._3oh-._58nk";
 let messageBox = undefined;
 
+// Called when fb opened for the first time. Will update all messages to the correct image
 function updateAll() {
 	emotes.forEach(function (emote) {
 		$(fbMsgSel).each(function(i) {	
@@ -32,30 +33,52 @@ function updateAll() {
 	})
 }
 
+//Called to update all spans in a newly loaded div
+function updateDiv(div) {
+	$(div).find(fbMsgSel).each(function (i) {
+		updateSpan($(this))
+	})
+}
+
+//Called to update a span
 function updateSpan(span) {
 	emotes.forEach(function (emote) {
 		replaced = $(span).html().replaceAll(emote.keyword, emote.imgElement);
 		$(span).html(replaced);
 	})
 }
-	
 
-function subscriber (mutations) {
+//Gets the last span from a div (when a new message has been typed)
+function getLastMsgFromDiv(div) {
+	return div.lastChild.firstChild.lastChild.firstChild
+}
+
+function message_handler (mutations) {
 	mutations.forEach((mutation) => {
 
-		//Replace the stuff in span
-		let span = mutation.target.childNodes[1].firstChild;
-		updateSpan(span);
+		// Added new chat div
+		if ($(mutation.target).hasClass("_41ud")) {
+
+			if ($(mutation.addedNodes[0]).hasClass("_ih3")) {
+				// If loaded a whole div
+				updateDiv(mutation.target)
+
+			} else if ($(mutation.addedNodes[0]).hasClass("clearfix")) { // Having multiple classes in here doesn't work
+				// If loaded a single message
+				let span = getLastMsgFromDiv(mutation.target)
+				updateSpan(span);
+			}
+		}
 	});
 }
 
 
 //Attach observer onto the message div when it loads
 const config = {
-	attributes: true,
+	childList: true,
 	subtree: true
 };
-const observer = new MutationObserver(subscriber)
+const observer = new MutationObserver(message_handler)
 waitLoad = setInterval(function() {
 	try {
 		messageBox = $("div.uiScrollableAreaContent")[2].firstChild.childNodes[2];
@@ -64,8 +87,13 @@ waitLoad = setInterval(function() {
 
 	if (messageBox != undefined) {
 		updateAll();
+		console.log(messageBox)
 		observer.observe(messageBox, config);
 		clearInterval(waitLoad);
 	}
 }, 1*1000)
+
+
+// If added a whole div, gives addednode: 	h5._ih3._-ne
+// If just added an end part, gives 		div.clearfix._o46._3erg._3i_m._nd_.direction_ltr.text_align_ltr
 
